e693c4dfc3ab6b5d131095f2f8207bf914a0ebaa
Sela-kren <127387908+Sela-kren@users.noreply.github.com>
2023-11-24 09:55:15 +0700

    vue with tailwind
    setup about vue 3 with tailwind

---
+++ e-commerce/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>hello this is me jeff</div>
+  <div class="bg-red-200 text-3xl sticky top-0 w-full">hello world</div>
+  <div class=" h-[2000px]">
+    <p>hello</p>
+    <p>my name is leang</p>
+  </div>
+
+</template>
+
+ 
