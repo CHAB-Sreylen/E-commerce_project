952659136a53a9cad725fb72e76db2113302ff93
Sela-kren <127387908+Sela-kren@users.noreply.github.com>
2024-01-04 23:12:33 +0700

    some patch

--- e-commerce/src/router/index.js
+++ e-commerce/src/router/index.js
@@ -29,9 +29,7 @@ const router = createRouter({
     {
       path: '/ProductDetail',
       name: 'ProductDetail',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+  
       component: () => import('../views/ProductDetail.vue')
     },
   ]
