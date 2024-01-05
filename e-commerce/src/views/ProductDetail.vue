88c67333fc46183a456c35e94c66a4c6931c3cb0
Sela-kren <127387908+Sela-kren@users.noreply.github.com>
2024-01-05 14:51:37 +0700

    product detail cover

--- e-commerce/src/views/ProductDetail.vue
+++ e-commerce/src/views/ProductDetail.vue
@@ -2,9 +2,10 @@
     <div class=" w-full h-[40px] flex justify-end items-center "><p class="mr-20 text-[#7E7676]"> Sign in / Register </p></div>
     <div class=" w-full flex justify-center items-center border-y border-[#B16969] py-1 "><img class="h-[124px] w-[279px]" src="../assets/envylogo.svg" alt=""></div>
     <Navbar class="sticky top-0 z-50"></Navbar>
-    <div class="h-fit w-full flex flex-col gap-[19px] justify-around px-2">
+    <div class="h-fit w-full flex flex-col gap-[19px] justify-around">
+        <ProductTheme></ProductTheme>
         <div class="w-full h-fit bg-green-50 flex flex-col">
-            
+
         </div>
         <div class="w-full h-5 bg-red-50">2</div>
         <div class="w-full h-5 bg-gray-50">3</div>
@@ -13,11 +14,12 @@
 
 <script>
     import Navbar from '../components/Navbar.vue';
-
+    import ProductTheme from '../components/ProductTheme.vue';
     export default{
         
         components:{
             Navbar,
+            ProductTheme,
         }
     }
 </script>
