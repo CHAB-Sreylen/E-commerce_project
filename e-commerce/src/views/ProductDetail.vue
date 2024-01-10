e51a9a7240dd11654368972d2289a19be692dd21
Sela-kren <127387908+Sela-kren@users.noreply.github.com>
2024-01-10 11:23:45 +0700

    product detail
    product detail update

--- e-commerce/src/views/ProductDetail.vue
+++ e-commerce/src/views/ProductDetail.vue
@@ -4,8 +4,6 @@
     </router-link>    
     <div class=" w-full flex justify-center items-center border-y border-[#B16969] py-1 "><img class="h-[124px] w-[279px]" src="../assets/envylogo.svg" alt=""></div>
     <Navbar class="sticky top-0 z-50"></Navbar>
-<<<<<<< Updated upstream
-=======
     <div class="h-fit w-full flex flex-col gap-[19px] justify-around">
         <ProductTheme :coverPage="PDT.Page" :detailImg="PDT.image"></ProductTheme>
         <ProductDetailBox></ProductDetailBox>
@@ -15,32 +13,25 @@
         <div class="w-full h-5 bg-red-50">2</div>
         <div class="w-full h-5 bg-gray-50">3</div>
     </div>
->>>>>>> Stashed changes
 </template>
 
 <script>
     import Navbar from '../components/Navbar.vue';
-<<<<<<< Updated upstream
-=======
     import ProductTheme from '../components/ProductTheme.vue';
     import ProductDetailBox from '../components/ProductDetailBox.vue';
 
     import {useEStore} from '../stores/eStore';
     import {mapState} from 'pinia';
->>>>>>> Stashed changes
 
     export default{
         
         components:{
             Navbar,
-<<<<<<< Updated upstream
-=======
             ProductTheme,
             ProductDetailBox
         },
         computed: {
             ...mapState(useEStore,['PDT'])
->>>>>>> Stashed changes
         }
     }
 </script>
